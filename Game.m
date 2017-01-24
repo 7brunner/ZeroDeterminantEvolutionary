5605d9c52b08f2dc278b071d6c57df3284a135ca
7brunner <7brunner@gmx.at>
2017-01-24 10:32:45 +0000

    Tournaments
    Game: runNGenerationTournament runs N tournaments with selection after
    each round

--- Game.m
+++ Game.m
@@ -11,9 +11,14 @@
 classdef Game
     
     properties
-        Payoffs1
-        Payoffs2
-        Population
+        Payoffs1        % 2x2 Payoff matrix for player 1
+        Payoffs2        % 2x2 Payoff matrix for player 2
+        Population      % cell array of players
+        MatchingType    % string either Random, Close or Far
+        PlayerTypes     % cell array of player types
+        PopulationShareHistory
+        PlayerTypeAvgFitnessHistory
+        LastGeneration
     end
     
     methods
@@ -54,6 +59,20 @@ classdef Game
                 
                 obj.Payoffs2 = Payoffs2;
             end
+            
+            %%%
+            % Check if matching type string passed, otherwise set to Random
+            if length(varargin)<=2
+                obj.MatchingType = 'Random';
+            else
+                MatchingType = varargin{3};
+                if ~ismember(MatchingType,{'Random','Close','Far'})
+                    error('Matching type must be one of Random, Close or Far')
+                end
+                obj.MatchingType = MatchingType;
+            end
+            
+            obj.LastGeneration = 0;
         end
         
         %% Play one round method
@@ -105,17 +124,100 @@ classdef Game
                 
                 Population{i} = eval([strPlayerType]);
             end
-            
+                        
             obj.Population = Population;
+            
+            %%%
+            % Store class names of player types
+            PlayerTypes = cellfun(@(x) x(1:(strfind(x,'(')-1)),PlayerTypes,'UniformOutput',false);
+            obj.PlayerTypes = PlayerTypes;
         end
         
-        %% Run tournament
-        % Loop over all players and match them with random opponent
+        %% Run one generation tournament
+        % Loop over all players and match them with opponent according to
+        % defined matching type
+        function obj = runOneGenerationTournament(obj,RoundsToPlay)
+            for i = 1:length(obj.Population(:,1))
+                Player1 = obj.Population{i,1};
+                [Player2,locPlayer2] = obj.matchOpponent(i);
+                [Player1,Player2] = playNRounds(obj,RoundsToPlay,Player1,Player2);
+                
+                obj.Population{i,1} = Player1;
+                obj.Population{locPlayer2,1} = Player2;
+            end
+        end
         
-        %% Create next generation
+        %% Match opponent method
+        % Matches one opponent according to defined matching type
+        function [Player2,locPlayer2] = matchOpponent(obj,locPlayer1)
+            switch obj.MatchingType
+                case 'Random'
+                    vecPositions = 1:length(obj.Population(:,1));
+                    vecPositions(locPlayer1) = [];
+                    locPlayer2 = vecPositions(ceil(rand()*length(vecPositions)));
+                    Player2 = obj.Population{locPlayer2,1};
+            end
+        end
+        
+        %% Run n generation tournament
+        % Call run one generation tournament and generate next generation n
+        % times
+        function obj = runNGenerationTournament(obj,NGenerations,RoundsPerGeneration)
+            obj.LastGeneration = 0;
+            
+            %%%
+            % Create vectors containing history 
+            obj.PopulationShareHistory = nan(NGenerations,length(obj.PlayerTypes));
+            obj.PlayerTypeAvgFitnessHistory = nan(NGenerations,length(obj.PlayerTypes));
+            
+            %%%
+            % Loop over generations
+            for i = 1:NGenerations
+                obj = runOneGenerationTournament(obj,RoundsPerGeneration);
+                obj = createNextGeneration(obj);
+            end
+        end
+    end
+    
+    methods(Access = protected)
+        %% Create next generation method
         % Store share and average scores of each player type in current
         % generation and sample new generation with probablity proportional
         % to average score
+        function obj = createNextGeneration(obj)
+            obj.LastGeneration = obj.LastGeneration + 1;
+            PlayerClasses = cellfun(@class,obj.Population(:,1),'UniformOutput',false);
+            AvgFitnesses = cellfun(@(x) x.AveragePayoffHistory(end),obj.Population);
+            
+            for i = 1:length(obj.PlayerTypes)
+                %%%
+                % Store shares of player type
+                strPlayerType = obj.PlayerTypes{i};
+                pos = ismember(PlayerClasses,strPlayerType);
+                obj.PopulationShareHistory(obj.LastGeneration,i) = ...
+                    sum(pos) / length(obj.Population(:,1));
+                
+                %%%
+                % Store average fitness of player type
+                obj.PlayerTypeAvgFitnessHistory(obj.LastGeneration,i) = ...
+                    mean(AvgFitnesses(pos));
+            end
+            
+            %%%
+            % Sample new population and reset player history
+            NewPopulation = cell(size(obj.Population));
+            SelectionProbability = cumsum(AvgFitnesses/sum(AvgFitnesses));
+            for i = 1:length(obj.Population(:,1))
+                newLoc = find(SelectionProbability<rand(),1,'last');
+                if isempty(newLoc); newLoc = 1; end
+                NewPopulation{i,:} = obj.Population{newLoc,:};
+                NewPlayer = NewPopulation{i,1};
+                NewPlayer = NewPlayer.resetHistory;
+                NewPopulation{i,1} = NewPlayer;
+            end
+            
+            obj.Population = NewPopulation;
+        end
     end
     
 end
